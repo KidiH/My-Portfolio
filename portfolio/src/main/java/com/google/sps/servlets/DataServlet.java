7412d4ad12673cbf9cf7e6f1006f3390beba32e2
KidiH <kalemu@college.harvard.edu>
2020-06-03 12:22:49 +0000

    Updates the doGet method to take in the comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,7 +21,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
 import java.util.Date;
 
@@ -29,36 +28,19 @@ import java.util.Date;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private List<String> proverbs;
+  
+  private List<String> comment = new ArrayList<>(); 
+  
 
-// Hardcoded list of the random proverbs
-    @Override
-  public void init() {
-    proverbs = new ArrayList<>();
-    proverbs.add(
-        "A single stick may smoke, but it will not burn");
-    proverbs.add(
-        "Do not blame God for having created the tiger, but thank Him for not giving it wings");
-    proverbs.add(
-        "Fifty lemons are a load for one person, but for fifty persons they are perfume.");
-    proverbs.add(
-        "It is foolhardy to start a fire just to see the flames.");
-    }
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String proverb = proverbs.get((int) (Math.random() * proverbs.size()));
-
-    // Convert the server stats to JSON
     
-    String json = convertToJson(proverbs);
-
-    // Send the JSON as the response
-    response.setContentType("application/json;");
+    String json = new Gson().toJson(comment);
+    response.setContentType("application/json");
     response.getWriter().println(json);
   }
-  private String convertToJson(List proverbs) {
-    Gson gson = new Gson();
-    String json = gson.toJson(proverbs);
-    return json;
-  }
+
+  
+
+  
 }
