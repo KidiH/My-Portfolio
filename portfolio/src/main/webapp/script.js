6f97b90602dc9593c9ab67c3b242b2b180474d2b
KidiH <kalemu@college.harvard.edu>
2020-06-04 02:52:23 +0000

    Add input tab for number of comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -58,13 +58,14 @@ async function getRandomProverb(){
     document.getElementById('proverb-container').innerText = proverb;
 }
 
-async function printComments() {
+function printComments() {
   
     // Fetch comments from the server
     
-    fetch('/data').then(response => response.json()).then((comment) => {
+    
+    fetch('/data?max='+document.getElementById('quantity').value).then(response => response.json()).then((comment) => {
       const commentElements = document.getElementById('user-message');
-
+        console.log(comment);
       // Runs a loop for each content of the comments array to be printed out
       comment.forEach((comments) => {
         commentElements.appendChild(createListElement(comments));
