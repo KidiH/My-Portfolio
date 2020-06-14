7f2b9bfd4c447eec11503cc5b08c1e4376667be8
KidiH <kalemu@college.harvard.edu>
2020-06-14 15:21:12 +0000

    Wrote a query to pass the intitial tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,45 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    List<TimeRange> occupiedTime = new ArrayList<TimeRange>();
+    List<TimeRange> eventAttendees = new ArrayList<TimeRange>();
+    Collection<TimeRange> timeOptions = new ArrayList<TimeRange>();
+    int endTimeOption = TimeRange.START_OF_DAY;
+    int startTimeOption = TimeRange.START_OF_DAY;
+    
+    // Get all the time ranges the requested attendees are occupied on 
+    for (Event event : events) {
+      if (!Collections.disjoint(event.getAttendees(), request.getAttendees())) {
+        occupiedTime.add(event.getWhen());
+      }
+    }
+ 
+    // Sort the occupied times in order of start time
+    Collections.sort(occupiedTime, TimeRange.ORDER_BY_START);
+ 
+    // Case 1: If there is vacant time between an event's start time 
+    //and the previous event's end time   
+    for (TimeRange time : occupiedTime) { 
+      if (time.start() >= endTimeOption && (time.start() - endTimeOption) >= request.getDuration()) { 
+        timeOptions.add(TimeRange.fromStartEnd(endTimeOption, time.start(), false));
+      }
+      if (time.end() >= endTimeOption) {
+        endTimeOption = time.end();
+      }
+    }
+    // Case 2: If there is vacant time between an event's end time
+    //and the next event's start time  
+    if ((TimeRange.END_OF_DAY - endTimeOption) >= request.getDuration()) {
+      timeOptions.add(TimeRange.fromStartEnd(endTimeOption, TimeRange.END_OF_DAY, true));
+    }
+    return timeOptions;
   }
 }
